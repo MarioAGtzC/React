61efc623be163544aff4f19cf82d9fd3b81ee7c2
Marioagtzc <marioagtzc@hotmail.com>
2020-09-10 16:57:49 -0500

    end of intro js

---
+++ 02-intro-javascript/src/bases/11-async-await.js
@@ -0,0 +1,21 @@
+// const getImagenPromesa = () => new Promise(resolve => resolve('https://ikkode.com'))
+// getImagenPromesa().then(console.log);
+
+const getImagen = async() => {
+    try {
+        const apiKey = '6cGGpnjtfQ61fjSELEu2ylLkHO8Eio80';
+        const resp = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+        const {data} = await resp.json();
+    
+        const {url} = data.images.original;
+    
+        const img = document.createElement('img');
+        img.src = url;
+        document.body.append(img);
+    } catch (error) {
+        // manejo del error
+        console.error(error);
+    }
+}
+
+getImagen();
